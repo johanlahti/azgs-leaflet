955b07cb9e02c5c4c9d6fc904c100d8923f91d28
johanlahti <info@lahtitude.se>
2014-04-14 16:42:05 +0200

    bug fixes with converting projection and implementation of event trigger "select" on feature click.

--- js/azgs-leaflet/GeoJSON.WFS.js
+++ js/azgs-leaflet/GeoJSON.WFS.js
@@ -3,15 +3,31 @@ L.GeoJSON.WFS = L.GeoJSON.extend({
 	CLASS_NAME: "L.GeoJSON.WFS",
 	 
 	options: {
-		uniqueAttr: "",
+		uniqueKey: "",
 		noBindZoom: true,
-		noBindDrag: false
+		noBindDrag: false,
+		params: {
+				typeName: null, // required
+				service: "WFS",
+				version: "1.1.0",
+				request: "GetFeature",
+				srsName: "EPSG:4326",
+				format: "text/geojson",
+				maxFeatures: 10000,
+				outputFormat: "json"
+		},
+		selectStyle: {
+			weight: 5,
+	        color: '#00FFFF',
+	        opacity: 1
+		}
 	},
 
 	initialize: function(serviceUrl, options) {
 		options = options || {};
 		
-		var featureType = options.featureType; // required
+		
+		options = $.extend(true, {}, this.options, options);
 		
 		L.GeoJSON.prototype.initialize.call(this, null, options);
 		
@@ -20,67 +36,52 @@ L.GeoJSON.WFS = L.GeoJSON.extend({
 			this.proxy = options.proxy || L.GeoJSON.WFS.proxy || null;
 		}
 		
-		var wfsVersion = options.wfsVersion || "1.1.0";
-		this.getFeatureUrl = serviceUrl;   //+ "?request=GetFeature&outputformat=json&version=" + wfsVersion + "&typeName=" + featureType;
+		this.getFeatureUrl = serviceUrl;
 		
-		this.params = {
-				typeName: featureType,
-				service: "WFS",
-				version: wfsVersion,
-				request: "GetFeature",
-				srsName: "EPSG:4326",
-				format: "text/geojson",
-				maxFeatures: 10000,
-				outputFormat: "json"
-		};
-		
-//		if (options.filter && options.filter instanceof DateFilter) { this.getFeatureUrl += "&CQL_FILTER=" + options.filter.cql; }
-		
-		
-		this.on("featureparse", function(e) {
-			if (e.geometryType != 'Point' && e.geometryType != 'MultiPoint') {
-				if (options.style) {
-					e.layer._originalStyle = options.style;
-					e.layer.setStyle(options.style);
-				} else if (options.filteredStyles) {
-					var fld = options.filteredStyles.propName;
-					var itemVal = e.properties[fld];
-					var style = L.Util.extend({}, options.filteredStyles['default'], options.filteredStyles.styles[itemVal]); 
-					e.layer._originalStyle = style;
-					e.layer.setStyle(style);
-				}
-			}
-			if (options.popupObj && options.popupOptions) {
-				e.layer.on("click", function(evt) {
-					e.layer._map.openPopup(options.popupObj.generatePopup(e, options.popupOptions));
-					if (options.popupFn) { options.popupFn(e); }
-				});			
-			}
-			else if (options.popupFld && e.properties.hasOwnProperty(options.popupFld)) {
-				e.layer.bindPopup(e.properties[options.popupFld], { maxWidth: 600 });
-			}
-			if (options.hoverObj || options.hoverFld) {
-				e.layer.on("mouseover", function(evt) {
-					hoverContent = options.hoverObj ? options.hoverObj.generateContent(e) : e.properties[options.hoverFld] || "Invalid field name" ;
-					hoverPoint = e.layer._map.latLngToContainerPoint(evt.target._latlng);
-					e.layer._hoverControl = new L.Control.Hover(hoverPoint, hoverContent);
-					e.layer._map.addControl(e.layer._hoverControl);	
-				});
-				e.layer.on("mouseout", function(evt) {
-					e.layer._map.removeControl(e.layer._hoverControl);
-				});
-			}
-			if (options.hoverColor) {
-				e.layer.on("mouseover", function(evt) {
-					var hoverStyle = L.Util.extend({}, e.layer._originalStyle, { stroke: true, color: options.hoverColor, weight: 3 });
-					e.layer.setStyle(hoverStyle);
-				});
-				e.layer.on("mouseout", function(evt) {
-					e.layer.setStyle(e.layer._originalStyle);
-				});
-			}
-			if (e.layer instanceof L.Marker.AttributeFilter) { e.layer.setIcon(e); }
-		});
+//		this.on("featureparse", function(e) {
+//			if (e.geometryType != 'Point' && e.geometryType != 'MultiPoint') {
+//				if (options.style) {
+//					e.layer._originalStyle = options.style;
+//					e.layer.setStyle(options.style);
+//				} else if (options.filteredStyles) {
+//					var fld = options.filteredStyles.propName;
+//					var itemVal = e.properties[fld];
+//					var style = L.Util.extend({}, options.filteredStyles['default'], options.filteredStyles.styles[itemVal]); 
+//					e.layer._originalStyle = style;
+//					e.layer.setStyle(style);
+//				}
+//			}
+//			if (options.popupObj && options.popupOptions) {
+//				e.layer.on("click", function(evt) {
+//					e.layer._map.openPopup(options.popupObj.generatePopup(e, options.popupOptions));
+//					if (options.popupFn) { options.popupFn(e); }
+//				});			
+//			}
+//			else if (options.popupFld && e.properties.hasOwnProperty(options.popupFld)) {
+//				e.layer.bindPopup(e.properties[options.popupFld], { maxWidth: 600 });
+//			}
+//			if (options.hoverObj || options.hoverFld) {
+//				e.layer.on("mouseover", function(evt) {
+//					hoverContent = options.hoverObj ? options.hoverObj.generateContent(e) : e.properties[options.hoverFld] || "Invalid field name" ;
+//					hoverPoint = e.layer._map.latLngToContainerPoint(evt.target._latlng);
+//					e.layer._hoverControl = new L.Control.Hover(hoverPoint, hoverContent);
+//					e.layer._map.addControl(e.layer._hoverControl);	
+//				});
+//				e.layer.on("mouseout", function(evt) {
+//					e.layer._map.removeControl(e.layer._hoverControl);
+//				});
+//			}
+//			if (options.hoverColor) {
+//				e.layer.on("mouseover", function(evt) {
+//					var hoverStyle = L.Util.extend({}, e.layer._originalStyle, { stroke: true, color: options.hoverColor, weight: 3 });
+//					e.layer.setStyle(hoverStyle);
+//				});
+//				e.layer.on("mouseout", function(evt) {
+//					e.layer.setStyle(e.layer._originalStyle);
+//				});
+//			}
+//			if (e.layer instanceof L.Marker.AttributeFilter) { e.layer.setIcon(e); }
+//		});
 	},
 	
 	onAdd: function(map) {
@@ -128,7 +129,7 @@ L.GeoJSON.WFS = L.GeoJSON.extend({
 	addData: function (geojson) {
 		var features = L.Util.isArray(geojson) ? geojson : geojson.features,
 		    i, len, feature, fid,
-		    uniqueAttr = this.options.uniqueAttr;
+		    uniqueKey = this.options.uniqueKey;
 		
 		if (features) {
 			var featureIndexes = this._featureIndexes,
@@ -137,10 +138,10 @@ L.GeoJSON.WFS = L.GeoJSON.extend({
 				// Only add this if geometry or geometries are set and not null
 				feature = features[i];
 				var uniqueVal = "";
-				if (uniqueAttr) {
-					uniqueAttrArr = uniqueAttr.split(",");
+				if (uniqueKey) {
+					uniqueKeyArr = uniqueKey.split(",");
 					var props = feature.properties;
-					$.each(uniqueAttrArr, function(i, val) {
+					$.each(uniqueKeyArr, function(i, val) {
 						uniqueVal += (""+props[val]);
 					});
 				}
@@ -182,10 +183,52 @@ L.GeoJSON.WFS = L.GeoJSON.extend({
 		}
 		var bounds = this._map.getBounds();
 		this.getFeature(bounds, function() {
-			if (self.options.uniqueAttr === null) {
+			if (self.options.uniqueKey === null) {
 				self.clearLayers();
 			}
 			self.addData(self.jsonData);
+			
+			var html;
+			var onFeatureClick = $.proxy(function(evt) {
+				var f = evt.target.feature,
+					target = evt.target;
+				var key = self.options.uniqueKey || "-";
+				var paramVal = null;
+				var layerId = self.options.layerId || target.options.layerId;
+				if (key) {
+					var val;
+					if (key.split(",").length > 1) {
+						var keyArr = key.split(",");
+						val = f.properties[keyArr[0]] + "_" + f.properties[keyArr[1]];
+					}
+					else {
+						val = f.properties[key];						
+					}
+					if (layerId && val) {
+						paramVal = [layerId, key, val].join(":");
+					}
+				}
+				// TODO: temp hack. Get layer id some other way
+				self._map.fire("selected", {
+					layerType: "vector",
+					paramVal: paramVal,
+					layerId: layerId,
+					properties: f.properties
+				});					
+				self.eachLayer(function(lay) {
+					self.resetStyle(lay);
+				});
+				if (target.setStyle) {
+					target.setStyle(self.options.selectStyle);					
+				}
+			    if (self.bringToFront && !L.Browser.ie && !L.Browser.opera) {
+			    	self.bringToFront();
+			    }
+			    
+			}, this);
+			self.eachLayer(function(f) {
+				f.on("click", onFeatureClick);
+			});
 		});
 	},
 	
@@ -227,15 +270,15 @@ L.GeoJSON.WFS = L.GeoJSON.extend({
     },
 	
 	getFeature: function(bounds, callback) {
-		if (bounds && !this.params.filter) {
+		if (bounds && !this.options.params.filter) {
 			// Make a filter so that we only fetch features within current viewport.
 			// Don't use bbox if filter is specified (wfs does not support a combination)
-			
+			var reverseBbox = this.options.hasOwnProperty("reverseAxisBbox") ? this.options.reverseAxisBbox : this.options.reverseAxis;
 			if (this.options.inputCrs) {
 				bounds = this._projectBounds(bounds, "EPSG:4326", this.options.inputCrs);
-				this.params.srsName = this.options.inputCrs;
+				this.options.params.srsName = this.options.inputCrs;
 			}
-			this.params.bbox = this._boundsToBbox(bounds, this.options.reverseAxis);
+			this.options.params.bbox = this._boundsToBbox(bounds, reverseBbox);
 		}
 		
 		var url = this.proxy ? this.proxy + encodeURIComponent(this.getFeatureUrl) : this.getFeatureUrl;
@@ -243,18 +286,19 @@ L.GeoJSON.WFS = L.GeoJSON.extend({
 			this.xhr.abort();
 			this.xhr = null;
 		}
+		this.fire("loading", {layer: this});
 		this.xhr = $.ajax({
 			url: url,
 			type: "POST",
-			data: this.params,
+			data: this.options.params,
 			context: this,
 			success: function(response) {
 				if (response.type && response.type == "FeatureCollection") {
 					this.jsonData = response;
-					this.toGeographicCoords(this.options.inputCrs || "EPSG:3857");
+					this.toGeographicCoords(this.options.inputCrs || "EPSG:4326");
 					callback();
 					this.fire("load", {layer: this});
-				}				
+				}
 			},
 			dataType: "json"
 		});
@@ -267,7 +311,7 @@ L.GeoJSON.WFS = L.GeoJSON.extend({
 	
 	toGeographicCoords: function(inputCrs) {
 		function projectPoint(coordinates /*[easting, northing]*/, inputCrs) {
-			var source = inputCrs || "EPSG:3857",
+			var source = inputCrs || "EPSG:4326",
 				dest = "EPSG:4326",
 				x = coordinates[0], 
 				y = coordinates[1];
@@ -281,7 +325,7 @@ L.GeoJSON.WFS = L.GeoJSON.extend({
 			switch (geom.type) {
 				case "Point":
 					coords = geom.coordinates;
-					if (!this.options.reverseAxis) {
+					if (this.options.reverseAxis) {
 						coords = this.swapCoords(coords);
 					}
 					projectedCoords = projectPoint(coords, inputCrs);
@@ -290,9 +334,9 @@ L.GeoJSON.WFS = L.GeoJSON.extend({
 				case "MultiPoint":
 					for (p=0, len2=geom.coordinates.length; p<len2; p++) {
 						coords = geom.coordinates[p];
-//						if (this.options.reverseAxis) {
-//							coords = this.swapCoords(coords);
-//						}
+						if (this.options.reverseAxis) {
+							coords = this.swapCoords(coords);
+						}
 						projectedCoords = projectPoint(coords, inputCrs);
 						features[i].geometry.coordinates[p] = projectedCoords;
 					}
@@ -305,7 +349,7 @@ L.GeoJSON.WFS = L.GeoJSON.extend({
 						coordsArr = geom.coordinates[p];
 						for (pp=0, len3=coordsArr.length; pp<len3; pp++) {
 							coords = coordsArr[pp];
-							if (!this.options.reverseAxis) {
+							if (this.options.reverseAxis) {
 								coords = this.swapCoords( coords );								
 							}
 							projectedCoords = projectPoint(coords, inputCrs);
@@ -318,7 +362,7 @@ L.GeoJSON.WFS = L.GeoJSON.extend({
 					coordsArr = geom.coordinates[0][0];
 					for (p=0, lenP=coordsArr.length; p<lenP; p++) {
 						coords = coordsArr[p];
-						if (!this.options.reverseAxis) {
+						if (this.options.reverseAxis) {
 							coords = this.swapCoords( coords );								
 						}
 						projectedCoords = projectPoint(coords, inputCrs);
